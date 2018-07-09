b652108329eac6f099790dd401ecde8873398766
Jaewook Oh <kyle.oh95@gmail.com>
2018-07-09 17:30:28 +0900

    create api.py

---
+++ test/load_testing/api.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import subprocess, datetime, time, logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+debug_logger = logging.getLogger(__name__)
+debug_logger.setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+api = Api(app)
+
+# Todo - classify the requests
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=6005)
