99798a95a1a561df3c90b3746af77e1c2e01796e
delightwook91 <delightwook@naver.com>
2018-07-12 18:17:58 +0900

    make to get listener

--- ssh_manager/listener.py
+++ ssh_manager/listener.py
@@ -1,6 +1,19 @@
 import fabfile
+import os
+import sys
+import subprocess
+
 
 def selectCase():
     pass
 def start():
-    pass
+    # os.system('fab -H 192.168.11.3 -p stack run_ssh')
+    print("############################################")
+    output = subprocess.Popen(['fab','-H', '192.168.11.3', '-p', 'stack','run_ssh'],stdout=subprocess.PIPE).stdout
+    result = output.read().strip()
+    output.close()
+    print("result  : : : ",result)
+    print("############################################")
+
+
+start()
