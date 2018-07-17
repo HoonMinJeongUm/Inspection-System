92f0317c1886b6e4b12cba1de2010d23cff24b84
Jaewook Oh <kyle.oh95@gmail.com>
2018-07-17 10:23:53 +0900

    plugin development

--- test/vnf_testing/plugins/plugin_locustio.py
+++ test/vnf_testing/plugins/plugin_locustio.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 # plugin_locustio.py
 
+from locust import HttpLocust, TaskSet, task
+import subprocess
 import logging
 LOG = logging.getLogger(__name__)
+
+
+# Bash Command Function for using bash shell
+def bash_command(cmd):
+    subprocess.Popen(['/bin/bash', '-c', cmd])
+
+
+def start():
+    cmd = 'locust -f locustfile.py'
+    bash_command(cmd)
--- test/vnf_testing/plugins/plugin_stressng.py
+++ test/vnf_testing/plugins/plugin_stressng.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 # plugin_stressng.py
 
+import subprocess
 import logging
 LOG = logging.getLogger(__name__)
+
+
+# Bash Command Function for using bash shell
+def bash_command(cmd):
+    subprocess.Popen(['/bin/bash', '-c', cmd])
+
+
+def start():
+    pass
