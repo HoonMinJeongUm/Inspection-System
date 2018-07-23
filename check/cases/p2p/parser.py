92022dd37e4702f246d896ddb93fa71a27d9cc20
delightwook91 <delightwook@naver.com>
2018-07-23 16:50:49 +0900

    call function

--- check/cases/p2p/parser.py
+++ check/cases/p2p/parser.py
@@ -2,4 +2,8 @@ from check.cases.base_parser import BaseParser
 
 class P2PParser(BaseParser):
     def parsing(self,result):
+        """
+        We need to add some code for chaged result
+        """
+        parsing_data = result
         return parsing_data
