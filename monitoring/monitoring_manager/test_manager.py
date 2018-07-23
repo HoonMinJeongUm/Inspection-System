d2d049e3bedf71c9edc813093be7256181d253e5
huisam <huisam@naver.com>
2018-07-23 13:37:23 +0900

    Test Environment Configuration

---
+++ monitoring/monitoring_manager/test_manager.py
@@ -0,0 +1,25 @@
+from monitoring_manager import MonitoringManager
+
+
+test_vnf = {'vnfd': {'tenant_id': u'd1e6919c73074d18ab6cd49a02e08391'},
+            'id': 'b9af3cb5-6e43-4b2c-a056-67bda3f71e1a'}
+test_kwargs = {'vdus': {'Zabbix_client':
+                            {'parameters':
+                                 {'application':
+                                      {'app_name': 'apache2',
+                                       'app_status': {'actionname': 'cmd',
+                                                      'cmd-action': 'sudo service \
+                                            apache2 restart',
+                                                      'condition': ['down']},
+                                       'ssh_username': 'ubuntu',
+                                       'app_port': 80,
+                                       'ssh_password': 'ubuntu'}},
+                             'name': 'zabbix',
+                             'zabbix_username': 'Admin',
+                             'zabbix_password': 'zabbix',
+                             'zabbix_server_ip': '192.168.56.102',
+                             'zabbix_server_port': 80,
+                             'mgmt_ip': '192.168.56.103'}}}
+
+test = MonitoringManager()
+test.add_to_appmonitor(test_vnf, test_kwargs)
