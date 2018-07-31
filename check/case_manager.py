414e919d139e5cc33d8bf7789e478d9353969019
delightwook91 <delightwook@naver.com>
2018-07-31 17:28:49 +0900

    init

--- check/case_manager.py
+++ check/case_manager.py
@@ -12,9 +12,6 @@ def call_case(case,data):
 
     CaseManager.invoke(casemodule.Client(),data)
 
-#driver_manager
-# data = [['192.168.11.3', '192.168.11.31'],['stack', 'stack'],'ping -c 3 -q 192.168.11.3']
-# call_case('P2P',data)
 
-data = [['192.168.11.3', '192.168.11.31'],['root', 'root'],'netstat -nap | grep ESTAB | wc -l']
-call_case('Bottleneck',data)
+data = [['192.168.11.3', '192.168.11.31'],['root', 'root'],'ping -c 3 -q 192.168.11.3']
+call_case('P2P',data)
