8cd9163c970a51f3c786a958b0257a334d2f3cad
Jaewook Oh <kyle.oh95@gmail.com>
2018-08-14 20:17:56 +0900

    testing component - plugin install scripts are modified

--- test/plugin_scripts/plugin_artillery_install.sh
+++ test/plugin_scripts/plugin_artillery_install.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y npm
+VERSION=v8.11.3
+DISTRO=linux-x64
+
+wget https://nodejs.org/dist/v8.11.3/node-$VERSION-$DISTRO.tar.xz
+
+sudo mkdir /usr/local/lib/nodeje
+sudo tar xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+sudo mv /usr/local/lib/nodejs/node-$VERSION-$DISTRO /usr/local/lib/nodejs/node-$VERSION
+
+sudo echo "export NODEJS_HOME=/usr/local/lib/nodejs/node-$VERSION/bin" >> $HOME/.profile
+sudo echo "export PATH=$NODEJS_HOME:$PATH" >> $HOME/.profile
+
+source $HOME/.profile
+
+sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+
 sudo npm install -g artillery
